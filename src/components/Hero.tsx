fbe2947258325897f969cc0bbd9d840d32180909
gtech29 <juan.rodriguez.517@my.csun.edu>
2025-05-26 09:14:31 -0700

    update hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,12 +4,12 @@ export default function Home() {
   return (
     <>
       {/* Hero Section */}
-      <section className="relative min-h-[100vh] bg-cover bg-center px-4 text-white">
+      <section className="relative min-h-[100vh] px-4">
         <div className="max-w-4xl mx-auto pt-20 text-center px-4">
           <h1 className="text-2xl md:text-2xl font-bold mb-4 text-black">
             Securing mission-critical systems at scale.
           </h1>
-          <p className="text-lg md:text-xl text-gray-300 mb-6">
+          <p className="text-lg md:text-xl text-gray-700 mb-6">
             Research & Development Engineer with hands-on experience in
             DevSecOps, containerized infrastructure, and real-time system
             integration for public sector environments.
